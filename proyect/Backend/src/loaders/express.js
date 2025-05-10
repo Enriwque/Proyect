0fd70dc2e86fd6a11e6f75c72077470f4a7b8b65
Enriwque <151953913+Enriwque@users.noreply.github.com>
2025-05-10 18:43:24 +0200

    inicio sesion 2

--- proyect/Backend/src/loaders/express.js
+++ proyect/Backend/src/loaders/express.js
@@ -4,10 +4,7 @@ import cors from 'cors';
  import routes from '../routes/index.js';
 
 export default (expressApp) => {
-expressApp.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+expressApp.use(cors());
  expressApp.use(express.json({ limit: '50mb' }));
 expressApp.use(express.urlencoded({ limit: '50mb', extended: true }));
  expressApp.use('/api/v1', routes);
